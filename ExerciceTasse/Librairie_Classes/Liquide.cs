c94ac22a3f08ce0627fc2b4e883edf482b84575d
kristopherMaltais <86839956+kristopherMaltais@users.noreply.github.com>
2021-10-27 13:04:59 -0500

    Add files via upload

---
+++ ExerciceTasse/Librairie_Classes/Liquide.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Librairie_Classes
+{
+    public class Liquide
+    {
+        // ** Champs ** //
+        private string m_nomLiquide;
+
+        // ** Propriétés ** //
+        
+
+        // ** Constructeurs ** //
+        public Liquide(string typeLiquide)
+        {
+            m_nomLiquide = typeLiquide;
+        }
+
+        // ** Méthodes ** //
+        public override string ToString()
+        {
+            return m_nomLiquide;
+        }
+    }
+}
