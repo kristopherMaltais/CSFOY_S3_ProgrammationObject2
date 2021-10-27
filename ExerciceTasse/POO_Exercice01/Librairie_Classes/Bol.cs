a2cc6ce845465dd91413e787716421c3f4f364d3
kristopherMaltais <86839956+kristopherMaltais@users.noreply.github.com>
2021-10-27 13:02:56 -0500

    Add files via upload

---
+++ ExerciceTasse/POO_Exercice01/Librairie_Classes/Bol.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Librairie_Classes
+{
+    public class Bol : Contenant
+    {
+        // ** Champs ** //
+
+        // ** Propriétés ** //
+
+        // ** Constructeurs ** //
+        public Bol()
+        {
+            base.m_volume = 350;
+            base.m_volumeMax = m_volume - (base.m_zoneTampon * m_volume);
+        }
+
+        // ** Méthodes ** //
+       
+    }
+}
---
+++ ExerciceTasse/POO_Exercice01/Tests_Unitaires/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Tests_Unitaires
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void AjouterLiquide ()
+        {
+
+        }
+    }
+}
