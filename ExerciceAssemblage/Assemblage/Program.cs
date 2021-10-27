dbd28211b89ee4b108e75be34356a22ea698b732
kristopherMaltais <86839956+kristopherMaltais@users.noreply.github.com>
2021-10-27 13:06:40 -0500

    Add files via upload

---
+++ ExerciceAssemblage/Assemblage/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using LibrairieClasses;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assemblage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Piece valveEau = new Piece("Valve à eau", 1234, 0481);
+            Piece laBase = new Piece("Base", 1387, 0474);
+            Piece systemeActivation = new Piece("Système d'activation", 1887, 0574);
+            Piece systemeActivation2 = new Piece("Système d'activation", 1887, 0574);
+            Piece couvercleLateral = new Piece("Couvercle latéral", 1987, 0374);
+            Piece couvercleLateral2 = new Piece("Couvercle latéral", 1987, 0374);
+            Piece pin = new Piece("pin", 1687, 0974);
+            Piece plug = new Piece("plug", 1657, 0964);
+            Piece poigne = new Piece("poigné", 3157, 2547);
+            Piece couvercleMonobloc = new Piece("Couvercle monobloc", 1257, 9874);
+            Piece visse = new Piece("Vise M8", 8757, 3774);
+
+
+            valveEau.AjouterPiece(laBase);
+            valveEau.AjouterPiece(systemeActivation);
+            valveEau.AjouterPiece(couvercleLateral);
+            valveEau.AjouterPiece(couvercleLateral);
+            
+            systemeActivation.AjouterPiece(pin);
+            systemeActivation.AjouterPiece(plug);
+            systemeActivation.AjouterPiece(poigne);
+            
+            couvercleLateral.AjouterPiece(couvercleMonobloc);
+            couvercleLateral.AjouterPiece(visse);
+            couvercleLateral.AjouterPiece(visse);
+            couvercleLateral.AjouterPiece(visse);
+            couvercleLateral.AjouterPiece(visse);
+            
+            couvercleLateral2.AjouterPiece(couvercleMonobloc);
+            couvercleLateral2.AjouterPiece(visse);
+            couvercleLateral2.AjouterPiece(visse);
+            couvercleLateral2.AjouterPiece(visse);
+            couvercleLateral2.AjouterPiece(visse);
+
+            //StringBuilder test = new StringBuilder();
+            //Console.WriteLine(valveEau.SortieHierarchiePiece(valveEau, test));
+
+            List<Piece> liste = new List<Piece>();
+
+            Dictionary<string, int> test2 = valveEau.SortieNomenclature(valveEau, liste);
+
+            foreach (KeyValuePair<string, int> element in test2)
+            {
+                Console.WriteLine($"{element.Key} --- {element.Value}");
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ ExerciceAssemblage/LibrairieClasses/Piece.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LibrairieClasses
+{
+    public class Piece
+    {
+        // ** Champs ** //
+        private string m_Description;
+        private int m_NumeroSerie;
+        private int m_Reference;
+        private List<Piece> m_ListeSousPiece;
+
+        //* Propriétés ** //
+
+        // ** Constructeur ** //
+        public Piece(string description, int numeroSerie, int reference)
+        {
+            m_Description = description;
+            m_NumeroSerie = numeroSerie;
+            m_Reference = reference;
+            m_ListeSousPiece = new List<Piece>();
+        }
+
+        // ** Méthodes ** //
+        public void AjouterPiece(Piece pieceAAjouter)
+        {
+            m_ListeSousPiece.Add(pieceAAjouter);
+        }
+
+        public string SortieHierarchiePiece(Piece piece, StringBuilder sortie, int compteIndentation = 0)
+        {
+
+            // Déclaration des variables
+            List<(Piece, int)> suivi = new List<(Piece, int)>();
+            
+            // Description de la pièce avec indentation selon son niveau
+            string indentation = new string(' ', compteIndentation);
+            sortie.Append(indentation + piece + "\n");
+
+            // Créer une liste que la pièce contient et ajuster le niveau d'indentation selon le niveau
+            suivi = piece.m_ListeSousPiece.Select(x => (x, compteIndentation + 2)).ToList();
+
+            // Récursivité de la méthode
+            foreach((Piece, int) element in suivi)
+            {
+                SortieHierarchiePiece(element.Item1, sortie, element.Item2);
+            }
+
+            return sortie.ToString();
+        }
+
+        public Dictionary<string, int> SortieNomenclature(Piece piece, List<Piece> listePieces)
+        {
+            List<Piece> suiviPiece = new List<Piece>();
+            listePieces.Add(piece);
+
+            suiviPiece = piece.m_ListeSousPiece.Select(x => x).ToList();
+            
+            foreach (Piece element in suiviPiece)
+            {
+                SortieNomenclature(element, listePieces);
+            }
+
+            return listePieces.GroupBy(x => x.m_Description).ToDictionary(x => x.Key, x => x.Count());
+            
+        }
+
+        public override string ToString()
+        {
+            return $"Pièce : {m_Description}, Part - #{m_Reference}, numéro série - #{m_NumeroSerie}";
+        }
+    }
+}
