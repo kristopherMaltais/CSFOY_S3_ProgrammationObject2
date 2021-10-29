80c9b4bc5be0c6c6d0d43896a54f47a3a7090481
Kristopher Maltais <2191621@csfoy.ca>
2021-10-29 10:06:58 -0500

    ajout exercice exception1 m3

---
+++ M3_TraitementException1/M3_TraitementException1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace M3_TraitementException1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
