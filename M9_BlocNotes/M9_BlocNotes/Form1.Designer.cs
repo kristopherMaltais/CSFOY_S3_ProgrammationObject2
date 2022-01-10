26ee00f761355eae927170452bd41f7f9c0536c3
Kristopher Maltais <2191621@csfoy.ca>
2022-01-10 11:43:11 -0500

    exercice introduction sur application graphique winform

--- M9_BlocNotes/M9_BlocNotes/Form1.Designer.cs
+++ M9_BlocNotes/M9_BlocNotes/Form1.Designer.cs
@@ -29,13 +29,109 @@ namespace M9_BlocNotes
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.msMenuPrincipal = new System.Windows.Forms.MenuStrip();
+            this.tsmiNouveau = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiOuvrir = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiEnregistrer = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiEnregistrerSous = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiQuitter = new System.Windows.Forms.ToolStripMenuItem();
+            this.tbTexte = new System.Windows.Forms.TextBox();
+            this.msMenuPrincipal.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // msMenuPrincipal
+            // 
+            this.msMenuPrincipal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiNouveau,
+            this.tsmiOuvrir,
+            this.tsmiEnregistrer,
+            this.tsmiEnregistrerSous,
+            this.tsmiQuitter});
+            this.msMenuPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.msMenuPrincipal.Name = "msMenuPrincipal";
+            this.msMenuPrincipal.Size = new System.Drawing.Size(800, 24);
+            this.msMenuPrincipal.TabIndex = 0;
+            this.msMenuPrincipal.Text = "menuStrip1";
+            // 
+            // tsmiNouveau
+            // 
+            this.tsmiNouveau.Name = "tsmiNouveau";
+            this.tsmiNouveau.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.tsmiNouveau.Size = new System.Drawing.Size(67, 20);
+            this.tsmiNouveau.Text = "Nouveau";
+            this.tsmiNouveau.Click += new System.EventHandler(this.tsmiNouveau_Click);
+            // 
+            // tsmiOuvrir
+            // 
+            this.tsmiOuvrir.Name = "tsmiOuvrir";
+            this.tsmiOuvrir.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.tsmiOuvrir.Size = new System.Drawing.Size(52, 20);
+            this.tsmiOuvrir.Text = "Ouvrir";
+            this.tsmiOuvrir.Click += new System.EventHandler(this.tsmiOuvrir_Click);
+            // 
+            // tsmiEnregistrer
+            // 
+            this.tsmiEnregistrer.Name = "tsmiEnregistrer";
+            this.tsmiEnregistrer.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.tsmiEnregistrer.Size = new System.Drawing.Size(75, 20);
+            this.tsmiEnregistrer.Text = "Enregistrer";
+            this.tsmiEnregistrer.Click += new System.EventHandler(this.tsmiEnregistrer_Click);
+            // 
+            // tsmiEnregistrerSous
+            // 
+            this.tsmiEnregistrerSous.Name = "tsmiEnregistrerSous";
+            this.tsmiEnregistrerSous.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Alt) 
+            | System.Windows.Forms.Keys.S)));
+            this.tsmiEnregistrerSous.Size = new System.Drawing.Size(102, 20);
+            this.tsmiEnregistrerSous.Text = "Enregistrer sous";
+            this.tsmiEnregistrerSous.Click += new System.EventHandler(this.tsmiEnregistrerSous_Click);
+            // 
+            // tsmiQuitter
+            // 
+            this.tsmiQuitter.Name = "tsmiQuitter";
+            this.tsmiQuitter.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Alt) 
+            | System.Windows.Forms.Keys.F4)));
+            this.tsmiQuitter.Size = new System.Drawing.Size(56, 20);
+            this.tsmiQuitter.Text = "Quitter";
+            this.tsmiQuitter.Click += new System.EventHandler(this.tsmiQuitter_Click);
+            // 
+            // tbTexte
+            // 
+            this.tbTexte.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbTexte.Location = new System.Drawing.Point(12, 27);
+            this.tbTexte.Multiline = true;
+            this.tbTexte.Name = "tbTexte";
+            this.tbTexte.Size = new System.Drawing.Size(776, 411);
+            this.tbTexte.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.tbTexte);
+            this.Controls.Add(this.msMenuPrincipal);
+            this.MainMenuStrip = this.msMenuPrincipal;
+            this.Name = "Form1";
+            this.Text = "Super bloc-notes";
+            this.msMenuPrincipal.ResumeLayout(false);
+            this.msMenuPrincipal.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip msMenuPrincipal;
+        private System.Windows.Forms.ToolStripMenuItem tsmiNouveau;
+        private System.Windows.Forms.ToolStripMenuItem tsmiOuvrir;
+        private System.Windows.Forms.ToolStripMenuItem tsmiEnregistrer;
+        private System.Windows.Forms.ToolStripMenuItem tsmiEnregistrerSous;
+        private System.Windows.Forms.ToolStripMenuItem tsmiQuitter;
+        private System.Windows.Forms.TextBox tbTexte;
     }
 }
 
--- M9_BlocNotes/M9_BlocNotes/Form1.cs
+++ M9_BlocNotes/M9_BlocNotes/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +13,57 @@ namespace M9_BlocNotes
 {
     public partial class Form1 : Form
     {
+        // ** Champs ** //
+        private string m_nomFichier;
+
+        // ** Propriétés ** //
+
+        // ** Constructeurs ** //
         public Form1()
         {
             InitializeComponent();
         }
 
+        // ** Méthodes ** //
+        private void tsmiNouveau_Click(object sender, EventArgs e)
+        {
+            tbTexte.Text = String.Empty;
+        }
+        private void tsmiOuvrir_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Fichier texte (*.txt)|*.txt";
+            DialogResult dr = ofd.ShowDialog();
+
+            if(dr == DialogResult.OK)
+            {
+                tbTexte.Text = File.ReadAllText(ofd.FileName);
+            }
+
+            m_nomFichier = ofd.FileName;
+        }
+        private void tsmiEnregistrer_Click(object sender, EventArgs e)
+        {
+            if(tbTexte.Text != String.Empty && m_nomFichier is not null)
+            {
+                File.WriteAllText(m_nomFichier, tbTexte.Text);
+            }
+        }
+        private void tsmiEnregistrerSous_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog sfd = new SaveFileDialog())
+            {
+                sfd.Filter = "Fichier texte (*.txt)|*.txt";
+
+                if(sfd.ShowDialog() == DialogResult.OK)
+                {
+                    File.WriteAllText(sfd.FileName, tbTexte.Text);
+                }
+            }
+        }
+        private void tsmiQuitter_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
