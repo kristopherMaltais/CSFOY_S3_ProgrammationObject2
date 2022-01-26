750205d37de3444d9ffe9ba5973e9759467532c2
Kristopher Maltais <2191621@csfoy.ca>
2022-01-26 11:05:47 -0500

    création de la solution de base et des deux fenêtres

---
+++ M10_CaisseEnregistreuse/LibrairieClasses/Facture.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LibrairieClasses
+{
+    public class Facture
+    {
+        // ** Champs ** //
+
+        // ** Propriétés ** //
+        public List<LigneFacture> LignesFacture { get; set; }
+        // ** Constructeurs ** //
+
+        // ** Méthodes ** //
+    }
+}
---
+++ M10_CaisseEnregistreuse/LibrairieClasses/LigneFacture.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrairieClasses
+{
+    public class LigneFacture
+    {
+        // ** Champs ** //
+
+        // ** Propriétés ** //
+        public string Description { get; set; }
+        public int Quantite { get; set; }
+        public decimal Prix { get; set; }
+        public decimal Total { get; set; }
+
+        // ** Constructeurs ** //
+
+        // ** Méthodes ** //
+    }
+}
